69d206d0f0839e6820e2592e7c3cf361707a8093
luniksss <nikolaeval316@gmail.com>
2025-09-13 23:37:41 +0300

    добавила CircleSquare на php

---
+++ docs/competitors/lab1/php/CircleSquare.php
@@ -0,0 +1,20 @@
+<?php
+
+define('INPUT_MESSAGE', "Введите радиус круга: ");
+define('RESULT_MESSAGE', "Площадь вашего круга равна: %s\n");
+define('ERROR_MESSAGE', "Пожалуйста, введите корректное положительное число для радиуса.\n");
+define('MIN_RADIUS', 0);
+
+echo INPUT_MESSAGE;
+$radius = trim(fgets(STDIN));
+
+if (is_numeric($radius) && $radius > MIN_RADIUS) 
+{
+    $area = pi() * $radius * $radius;
+    printf(RESULT_MESSAGE, $area);
+} 
+else 
+{
+    echo ERROR_MESSAGE;
+}
+?>
